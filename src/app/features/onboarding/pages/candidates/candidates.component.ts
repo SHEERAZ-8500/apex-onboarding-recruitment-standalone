247a5f67f02a8895e94add956945849c8fee8830
SHEERAZ ALI <sheraz.ali@inovabridge.com>
2026-02-12 18:29:18 +0500

    push

--- src/app/features/onboarding/pages/candidates/candidates.component.ts
+++ src/app/features/onboarding/pages/candidates/candidates.component.ts
@@ -294,7 +294,7 @@ export class CandidatesComponent implements OnInit {
     delete (this.candidate as any).designation;
     delete (this.candidate as any).category;
     delete (this.candidate as any).remarks;
-    if (this.title === 'edit' || this.title === 'view') {
+    if (this.title === 'edit' || this.title === 'create') {
       delete (this.candidate as any).candidate_attachment;
       delete (this.candidate as any).candidate_experience;
       delete (this.candidate as any).candidate_qualification;
